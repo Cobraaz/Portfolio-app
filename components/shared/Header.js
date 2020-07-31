e900f33b205b7313b63874d2db397919fdf9287b
Cobraaz <anujbansal077@gmail.com>
2020-08-01 00:07:26 +0530

    Added Secret page both servserside and client side

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -60,6 +60,12 @@ const Header = ({ user, loading }) => {
             <NavItem className="port-navbar-item">
               <BsNavLink href="/cv" title="Cv" />
             </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/secret" title="Secret" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/secretssr" title="SecretSSR" />
+            </NavItem>
           </Nav>
           <Nav navbar>
             {!loading && (
