ccdd535718e90c2b9c7f9fd7a73c8930b66a1d79
Cobraaz <anujbansal077@gmail.com>
2020-08-07 16:38:53 +0530

    imporved basic Css

--- components/shared/Masthead.js
+++ components/shared/Masthead.js
@@ -1,24 +1,11 @@
 import { Container, Row, Button } from "reactstrap";
 import { Slide } from "react-slideshow-image";
-import Particles from "react-particles-js";
 
 const slideImages = [
   "/images/home-bg.jpg",
   "/images/home-bg2.jpg",
   "/images/home-bg3.jpg",
 ];
-
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 13,
-      density: {
-        enable: true,
-        value_area: 1500,
-      },
-    },
-  },
-};
 
 const Masthead = ({ children }) => (
   <Slide
@@ -38,7 +25,6 @@ const Masthead = ({ children }) => (
           <Row>
             <div className="col-lg-8 col-md-10 mx-auto">
               <div className="site-heading">{children}</div>
-              <Particles className="particles" params={particlesOptions} />
             </div>
           </Row>
         </Container>
