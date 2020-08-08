d850f316e98c8cb5251fd338ccfdc6033470d14a
Cobraaz <anujbansal077@gmail.com>
2020-08-08 15:16:14 +0530

    decrease the size of images and roolback the masthead to its original form

--- components/shared/Masthead.js
+++ components/shared/Masthead.js
@@ -1,7 +1,5 @@
 import { Container, Row, Button } from "reactstrap";
 import { Slide } from "react-slideshow-image";
-import Particles from "react-particles-js";
-import { particlesOptionsBlogs } from "helpers";
 
 const slideImages = [
   "/images/home-bg.jpg",
@@ -9,69 +7,30 @@ const slideImages = [
   "/images/home-bg3.jpg",
 ];
 
-const Masthead = ({ children, imagePath }) => (
-  <>
-    <Slide
-      duration={5000}
-      arrows={false}
-      transitionDuration={2000}
-      pauseOnHover={false}
-    >
-      {slideImages.map((slideImage) => (
-        <div
-          className="masthead"
-          style={{ backgroundImage: `url(${slideImage})` }}
-        >
-          <div className="overlay"></div>
-          <Container>
-            <Row>
-              <div className="col-lg-8 col-md-10 mx-auto">
-                <div className="site-heading">{children}</div>
-              </div>
-            </Row>
-          </Container>
-          <Particles className="particles" params={particlesOptionsBlogs} />
-        </div>
-      ))}
-    </Slide>
-  </>
+const Masthead = ({ children }) => (
+  <Slide
+    duration={3000}
+    arrows={false}
+    transitionDuration={1400}
+    pauseOnHover={false}
+  >
+    {slideImages.map((slideImage, index) => (
+      <div
+        key={index}
+        className="masthead"
+        style={{ backgroundImage: `url(${slideImage})` }}
+      >
+        <div className="overlay"></div>
+        <Container>
+          <Row>
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="site-heading">{children}</div>
+            </div>
+          </Row>
+        </Container>
+      </div>
+    ))}
+  </Slide>
 );
 
 export default Masthead;
-
-// import { Container, Row, Button } from "reactstrap";
-// import { Slide } from "react-slideshow-image";
-
-// const slideImages = [
-//   "/images/home-bg.jpg",
-//   "/images/home-bg2.jpg",
-//   "/images/home-bg3.jpg",
-// ];
-
-// const Masthead = ({ children }) => (
-//   <Slide
-//     duration={3000}
-//     arrows={false}
-//     transitionDuration={1400}
-//     pauseOnHover={false}
-//   >
-//     {slideImages.map((slideImage, index) => (
-//       <div
-//         key={index}
-//         className="masthead"
-//         style={{ backgroundImage: `url(${slideImage})` }}
-//       >
-//         <div className="overlay"></div>
-//         <Container>
-//           <Row>
-//             <div className="col-lg-8 col-md-10 mx-auto">
-//               <div className="site-heading">{children}</div>
-//             </div>
-//           </Row>
-//         </Container>
-//       </div>
-//     ))}
-//   </Slide>
-// );
-
-// export default Masthead;
