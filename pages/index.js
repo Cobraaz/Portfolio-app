0c075c7edf245f178a5a70c96c0cebcf9de32c42
Cobraaz <anujbansal077@gmail.com>
2020-10-29 12:24:36 +0530

    Again changing name due to vercel failing

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { useGetUser } from "actions/user";
 import Particles from "react-particles-js";
 import { ROLES, particlesOptionsIndex } from "helpers";
 import Link from "next/link";
-import AlanAi from "components/AlanAi/index";
+import AlanAi from "components/AlanAi/AlanAi";
 
 const Index = () => {
   const [isFlipping, setIsFlipping] = useState(false);
