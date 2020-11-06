afaadd0af2ee34a17531c190f324d53f45d2b960
Cobraaz <anujbansal077@gmail.com>
2020-11-06 14:42:20 +0530

    Added framer-motion to about page

--- pages/about.js
+++ pages/about.js
@@ -3,41 +3,144 @@ import BaseLayout from "components/layouts/BaseLayout";
 import BasePage from "components/layouts/BasePage";
 import { useGetUser } from "actions/user";
 import { Row, Col } from "reactstrap";
+import { motion } from "framer-motion";
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      when: "beforeParent",
+      delay: 0.2,
+    },
+  },
+};
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      staggerChildren: 0.4,
+      mass: 1.4,
+      stiffness: 80,
+      damping: 13,
+      delay: 0.9,
+    },
+  },
+  exit: {
+    x: "-100vh",
+    transition: { ease: "easeInOut" },
+  },
+};
+
+const childVariantsHover = {
+  hover: {
+    scale: 1.1,
+    transition: {
+      duration: 0.5,
+      delay: 0.2,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.9,
+    },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: [1, 2, 2, 1, 1],
+    rotate: [0, 0, 270, 270, 0],
+    borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    transition: {
+      duration: 1.4,
+    },
+  },
+};
 
 const About = () => {
   const { data, loading } = useGetUser();
-
   useEffect(() => {
     return () => {
       window.__isAboutLoadeded = true;
     };
   }, []);
 
-  const createFadeInClass = () => {
+  const createContainerVariants = () => {
     if (typeof window !== "undefined") {
-      return window.__isAboutLoadeded ? "" : "fadein";
+      return window.__isAboutLoadeded ? "" : containerVariants;
     }
 
-    return "fadein";
+    return containerVariants;
   };
 
+  const createNextVariants = () => {
+    if (typeof window !== "undefined") {
+      return window.__isAboutLoadeded ? "" : nextVariants;
+    }
+
+    return nextVariants;
+  };
+
+  const createChildVariants = () => {
+    if (typeof window !== "undefined") {
+      return window.__isAboutLoadeded ? "" : childVariants;
+    }
+
+    return childVariants;
+  };
   return (
     <BaseLayout user={data} loading={loading} navClass="with-bg-about">
       <BasePage className="about-page">
         <Row className="">
           <Col md="6">
             <div className="left-side">
-              <h1 className={`title ${createFadeInClass()}`}>Hello, Welcome</h1>
-              <h4 className={`subtitle ${createFadeInClass()}`}>
-                To About Page
-              </h4>
-              <p className={`subsubTitle ${createFadeInClass()}`}>
-                Feel free to read short description about me.
-              </p>
+              <motion.h1
+                variants={createNextVariants()}
+                initial="hidden"
+                animate="visible"
+                className={`title`}
+              >
+                Hello, Welcome
+              </motion.h1>
+              <motion.div
+                variants={createContainerVariants()}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+              >
+                <h4 className={`subtitle`}>To About Page</h4>
+                <p className={`subsubTitle`}>
+                  Feel free to read short description about me.
+                </p>
+              </motion.div>
             </div>
           </Col>
           <Col md="6">
-            <div className={`subsubTitle ${createFadeInClass()} mt-3`}>
+            <motion.div
+              variants={createContainerVariants()}
+              initial="hidden"
+              animate="visible"
+              className={`subsubTitle mt-3`}
+            >
               <h5 style={{ fontWeight: "bolder" }}>
                 Hi, I’m Anuj Bansal. I'm a multi-talented human with over 6+
                 months of experiences in Reactjs and Node.js.
@@ -55,32 +158,37 @@ const About = () => {
                 skills and juggle different projects and roles with relative
                 ease.
               </p>
-            </div>
+            </motion.div>
           </Col>
           <Col md="12" className="">
-            <div style={{ textAlign: "center" }}>
-              <i
-                className={`ri-code-box-line code-icons ${createFadeInClass()}`}
-              ></i>
+            <motion.div
+              variants={createChildVariants()}
+              initial="hidden"
+              animate="visible"
+              whileHover="hover"
+              style={{ textAlign: "center" }}
+            >
+              <motion.div variants={buttonVariants} whileHover="hover">
+                <i className={`ri-code-box-line code-icons `}></i>
+              </motion.div>
+              <motion.div variants={childVariantsHover} whileHover="hover">
+                <h5 className={`code-title `}>Full-stack Developer</h5>
+                <div>
+                  <p>
+                    I like to code things from scratch, and enjoy bringing ideas
+                    to life in the browser.
+                  </p>
 
-              <h5 className={`code-title ${createFadeInClass()}`}>
-                Full-stack Developer
-              </h5>
-              <div className={`code-subsubtitle ${createFadeInClass()}`}>
-                <p>
-                  I like to code things from scratch, and enjoy bringing ideas
-                  to life in the browser.
-                </p>
-
-                <p style={{ color: "#7510f7", fontSize: "25px" }}>
-                  Languages I speak:
-                </p>
-                <p>
-                  HTML, CSS,Javascript, Reactjs, MongoDB, Express, Nodejs,
-                  Nextjs
-                </p>
-              </div>
-            </div>
+                  <p style={{ color: "#7510f7", fontSize: "25px" }}>
+                    Languages I speak:
+                  </p>
+                  <p>
+                    HTML, CSS,Javascript, Reactjs, MongoDB, Express, Nodejs,
+                    Nextjs
+                  </p>
+                </div>
+              </motion.div>
+            </motion.div>
           </Col>
         </Row>
       </BasePage>
